81e9a148be3b32f5874646fb2bc4f5f2c9e93638
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-03-07 20:53:53 -0500

    created users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_012139) do
+ActiveRecord::Schema.define(version: 2021_03_08_015229) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2021_03_08_012139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
