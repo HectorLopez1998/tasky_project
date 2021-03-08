c21554ec790b53003b92ab07cd7e6d3306fe9311
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-03-07 20:58:30 -0500

    created column for username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_015229) do
+ActiveRecord::Schema.define(version: 2021_03_08_015546) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_015229) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "username"
   end
 
 end
