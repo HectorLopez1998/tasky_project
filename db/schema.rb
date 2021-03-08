6a3ed729896e4acad9aafb3540902e54a17eee18
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-03-07 21:04:21 -0500

    added new columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_020000) do
+ActiveRecord::Schema.define(version: 2021_03_08_020246) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_03_08_020000) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
+    t.string "password_digest"
+    t.string "email"
   end
 
 end
