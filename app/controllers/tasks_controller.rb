c775108c2d02a8ebee0fa71c542c48cc4fbd8608
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-03-07 20:18:22 -0500

    created migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,49 +1,56 @@
 
-class TaskController < ApplicationController
+class TasksController < ApplicationController
+   
 
     get '/tasks' do 
         @tasks = Task.all 
         erb :'tasks/index'
     end
 
-    get '/tasks/new' do
-        @task = Task.new
-        erb :'tasks/new'
-   end
+    
 
-   get '/tasks/:id' do 
-    # @task = Task.find(params[:id])
-    find_task
-    erb :'tasks/show'
-  end
+     get '/tasks/new' do
+           @task = Task.new
+           erb :'tasks/new'
+      end
 
-  post '/tasks' do 
+      get '/tasks/:id' do 
+        # @task = Task.find(params[:id])
+        find_task
+        erb :'tasks/show'
+      end
+    
+
+      post '/tasks' do 
         
-    @task = Task.create(params)
-    redirect "/tasks/#{@task.id}"
-  end
+        @task = Task.create(params)
+        redirect "/tasks/#{@task.id}"
+      end
 
-  get '/tasks/:id/edit' do 
-    @task = Task.find_by(id:params[:id])
-    erb :'tasks/edit'
-  end
+      get '/tasks/:id/edit' do 
+        @task = Task.find_by(id:params[:id])
+        erb :'tasks/edit'
+      end
 
-  patch '/tasks/:id' do 
-    @task = Task.find_by(id:params[:id])
-    @task.update(title: params[:title], description: params[:description])
-    redirect "/tasks/#{@task.id}"
-  end
+      patch '/tasks/:id' do 
+        @task = Task.find_by(id:params[:id])
+        @task.update(title: params[:title], description: params[:description])
+        redirect "/tasks/#{@task.id}"
+      end
 
-  delete '/tasks/:id' do 
-    @task = Task.find_by(id:params[:id])
-    @task.destroy
-    redirect 'tasks'
-  end
+      delete '/tasks/:id' do 
+        @task = Task.find_by(id:params[:id])
+        @task.destroy
+        redirect 'tasks'
+      end
+    
+    
 
-
-
-private
+    private
     def find_task
         @task = Task.find_by(id:params[:id])
     end
+
+    
+
 end
