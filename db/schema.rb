79e338b74f4ed1a8c0eca4ddb94e45fe7710c619
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-03-07 20:22:37 -0500

    created and added category_id to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_012006) do
+ActiveRecord::Schema.define(version: 2021_03_08_012139) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_012006) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
