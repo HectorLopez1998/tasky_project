5a3e45e70d8c84fa9bac6b882ba137214198d6f3
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-03-07 21:57:04 -0500

    created login and signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 
 class UsersController < ApplicationController
 
+    get '/signup' do 
+        erb :"users/signup"
+    end
 
+    post '/signup' do 
+        user = User.new(params)
+
+        if user.username.blank? || user.email.blank? || user.name.blank? || user.password.blank?
+            redirect '/signup'
+        else 
+            user.save
+            
+            redirect '/tasks'
+        end 
+
+        
+    end
+    
+    get '/login' do 
+        erb :"users/login"
+    end
+
+    post '/login' do 
+    end
 end
